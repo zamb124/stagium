01843495c412b6af3c5593b53c3b7fcddce3a934
viktorshvedov <zambas124@gmail.com>
2019-09-01 14:27:17 +0300

    INITIAL

---
+++ applicants/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.http.response import HttpResponseRedirect, HttpResponse
+
+# Create your views here.
+def profile(request, id):##
+    return render(request, 'index_profile.html')
---
+++ skils/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SkilsConfig(AppConfig):
+    name = 'skils'
