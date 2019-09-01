4cc943ff0d8dca720a3cb6c5d04d856f9ee15750
viktorshvedov <zambas124@gmail.com>
2019-09-01 16:51:26 +0300

    INITIAL++

--- stagium/settings.py
+++ stagium/settings.py
@@ -32,8 +32,29 @@ ALLOWED_HOSTS = [
 
 
 # Application definition
-
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',
+    'social_core.backends.google.GoogleOpenId',
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.google.GoogleOAuth',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.yahoo.YahooOpenId',
+    'django.contrib.auth.backends.ModelBackend',
+)
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
+)
 INSTALLED_APPS = [
+'social_django',
     'quiz',
     'multichoice',
     'true_false',
@@ -70,6 +91,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
